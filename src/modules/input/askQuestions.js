a0aeca613aea0b77ef5af272494d2a0a112f3a48
adam.kelsall <adam.kelsall@accenture.com>
2016-09-12 16:06:27 +0100

    Appreciated the true beauty of ES7 promises
    - <3
    - No need to try/catch ES7 promises, a raw return await works too

--- src/modules/input/askQuestions.js
+++ src/modules/input/askQuestions.js
@@ -1,10 +1,5 @@
-import errorHandler from '../errorHandler';
 import inquirer from 'inquirer';
 
 export default async function (questions) {
-  try {
-    return await inquirer.prompt(questions);
-  } catch (err) {
-    errorHandler('asking questions', err);
-  }
+  return await inquirer.prompt(questions);
 }
