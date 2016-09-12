62d1ec3321a3b80a34d80764f3ea78b8beae0838
adam.kelsall <adam.kelsall@accenture.com>
2016-09-12 16:03:03 +0100

    errorHandler origin clarification
    Made sure that it is clear that template printed out the caught error

--- src/modules/errorHandler.js
+++ src/modules/errorHandler.js
@@ -4,7 +4,7 @@ export default function (task, err) {
   const punctuation = err ? ':' : '.';
 
   console.log(chalk.red('Oh no, error!'));
-  console.log(`Something went wrong while ${context}${punctuation}`);
+  console.log(`Something went wrong with templater while ${task}${punctuation}`);
   if (err) console.log(err);
 
   process.exit(1);
