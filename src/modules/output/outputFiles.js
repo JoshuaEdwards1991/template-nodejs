030f9747c382c15a2342d15f7d0b37917810a9ef
adam.kelsall <adam.kelsall@accenture.com>
2016-09-14 08:56:01 +0100

    Added error handling to outputFiles
    - Wrapped file writing in try/catch block

--- src/modules/output/outputFiles.js
+++ src/modules/output/outputFiles.js
@@ -1,11 +1,17 @@
+import errorHandler from '../errorHandler';
 import path from 'path';
 import { validateFiles } from '../validation';
 import { writeFile } from './fileSystem';
 
 export default async function writeFiles(files) {
-  validateFiles(files);
+  try {
+    validateFiles(files);
 
-  for (const file of files) {
-    await writeFile(path.resolve(file.path), file.content);
+    for (const file of files) {
+      await writeFile(path.resolve(file.path), file.content);
+    }
+
+  } catch (err) {
+    errorHandler('writing files', err);
   }
 }
